86c333c7ebc04753ff34bc822caa0bcc75c9e142
Julia Filippova <julia.94@bk.ru>
2022-06-15 19:25:12 +0300

    feat: add API key

--- news-API/src/components/controller/appLoader.js
+++ news-API/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '3606c2b491374a8bb8c5dc2514462510', // получите свой ключ https://newsapi.org/
         });
     }
 }
