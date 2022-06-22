f52bec906d82dda50de9f3a67bfa16b2c9559e85
Julia Filippova <julia.94@bk.ru>
2022-06-22 21:44:03 +0300

    feat: add pagination

--- news-API/src/components/view/sources/sources.ts
+++ news-API/src/components/view/sources/sources.ts
@@ -1,21 +1,53 @@
+import IData from '../iDataInterface';
 import './sources.css';
 
 class Sources {
     draw(data: Array<{[key:string]: string}>):void {
         const fragment:DocumentFragment = document.createDocumentFragment();
         const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+        let pagination = document.querySelector('.pagination') as HTMLElement;
 
-        data.forEach((item: {[key:string]: string}) => {
-            const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLTemplateElement;
+        let notesOnPage:number = 10;
+        let countOfItem:number = Math.ceil(data.length/notesOnPage)
 
-            sourceClone.querySelector('.source__item-name').textContent = item.name;
-            sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
+        for(let i:number = 1; i <= countOfItem; i++){
+            let li = document.createElement('li') as HTMLElement
+            li.innerText = String(i);
+            pagination.append(li)
+        }
 
-            fragment.append(sourceClone);
-        });
-
-        document.querySelector('.sources').append(fragment);
+        let list: NodeListOf<Element> = document.querySelectorAll('.pagination li');
+        showPage(list[0]);
+        console.log(list)
+        list.forEach(li => {
+              li.addEventListener('click', ():void =>{
+                showPage(li)
+            })
+        })
+          
+        function showPage(li: Element):void{
+            let active = document.querySelector('.pagination li.active') as HTMLElement
+            if(active){
+               active.classList.remove('active') 
+            }
+            li.classList.add('active')
+            let pageNum:number = +li.innerHTML ;
+            let start:number = (pageNum-1)* notesOnPage;
+            let end:number = start + notesOnPage;
+            let notes:Array<{[key:string]: string}> = data.slice(start, end)
+        
+            document.querySelector('.sources').innerHTML = ''
+            for(let item of notes){
+                const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLTemplateElement;
+        
+                sourceClone.querySelector('.source__item-name').textContent = item.name ;
+                sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
+                fragment.append(sourceClone);
+            }
+            document.querySelector('.sources').append(fragment);
+        }
     }
 }
+
 
 export default Sources;
