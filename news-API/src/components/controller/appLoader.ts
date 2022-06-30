a8dfd997d167ccd281797883328f82fba8c18455
Julia Filippova <julia.94@bk.ru>
2022-06-30 17:39:14 +0300

    fix: correct apiKey and height block news

--- news-API/src/components/controller/appLoader.ts
+++ news-API/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '5a800113d7ac4268a41eaefc09267c40', // получите свой ключ https://newsapi.org/  3606c2b491374a8bb8c5dc2514462510
+            apiKey: '3606c2b491374a8bb8c5dc2514462510', // получите свой ключ https://newsapi.org/  3606c2b491374a8bb8c5dc2514462510
         });
     }
 }
