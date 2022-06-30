d5ebd6f46c1ac596e99f4b0d9158da3a4798a120
Julia Filippova <julia.94@bk.ru>
2022-06-30 22:07:43 +0300

    fix: correct appLoader link and apiKey

--- news-API/src/components/controller/appLoader.ts
+++ news-API/src/components/controller/appLoader.ts
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '20a6cfce107d4cdea467e40d19e3b441', // получите свой ключ https://newsapi.org/  3606c2b491374a8bb8c5dc2514462510
+        super('https://nodenews.herokuapp.com/', {
+            apiKey: '3606c2b491374a8bb8c5dc2514462510', // получите свой ключ https://newsapi.org/  3606c2b491374a8bb8c5dc2514462510
         });
     }
 }
